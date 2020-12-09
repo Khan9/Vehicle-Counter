f91d9c0d62f5c1dbd5233a65fa944ebd9cd25b5c
Khan9 <mskhanuap@gmail.com>
2020-12-09 16:25:55 +0600

    committed

---
+++ main.py
@@ -0,0 +1,223 @@
+# import the necessary packages
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+import datetime
+import os
+from yolo import YOLO 
+from PIL import Image, ImageDraw, ImageFont
+from sort import *
+import tensorflow as tf
+from timeit import default_timer as timer
+
+
+import tensorflow.compat.v1.keras.backend as K
+import tensorflow as tf
+tf.compat.v1.disable_eager_execution()
+
+
+def main(yolo):
+    tracker = Sort()
+    memory = {}
+    line1 = [(350, 400), (900, 400)]
+    line2 = [(976,630), (1677,822)]    
+
+    
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-c", "--confidence", type=float, default=0.5,
+        help="minimum probability to filter weak detections")
+    ap.add_argument("-t", "--threshold", type=float, default=0.4,
+        help="threshold when applyong non-maxima suppression")
+    args = vars(ap.parse_args())
+
+    # Return true if line segments AB and CD intersect
+    def intersect(A,B,C,D):
+        return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
+
+    def ccw(A,B,C):
+        return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[0]-A[0])
+
+    # load the COCO class labels our YOLO model was trained on
+    #labelsPath = os.path.sep.join([args["yolo"], "classes.names"])
+    #LABELS = open(labelsPath).read().strip().split("\n")
+
+    # initialize a list of colors to represent each possible class label
+    np.random.seed(42)
+    COLORS = np.random.randint(0, 255, size=(200, 3),dtype="uint8")
+
+
+    #Put your video path here
+    vs = cv2.VideoCapture('test_data.mp4')
+    writer = None
+    (W, H) = (None, None)
+    
+    font = ImageFont.truetype("arial.ttf", 30)
+    
+    frameIndex = 0
+    car = 0
+    motor = 0
+    bus = 0
+    truck = 0
+    car2 = 0 
+    motor2 = 0
+    bus2 = 0
+    truck2 = 0
+    # loop over frames from the video file stream
+    prev_time = timer()
+    accum_time = 0
+    curr_fps = 0
+    while True:
+        # read the next frame from the file
+        (grabbed, frame) = vs.read()
+        
+        # if the frame was not grabbed, then we have reached the end
+        # of the stream
+        if not grabbed:
+            break
+
+        image = Image.fromarray(frame[...,::-1])
+        boxes, out_class, confidences, midPoint = yolo.detect_image(image)
+        image = np.asarray(image)
+
+        # and class IDs, respectively
+        classIDs = []
+        #classIDs.append(classID)
+
+        # apply non-maxima suppression to suppress weak, overlapping
+        # bounding boxes
+
+        idxs = cv2.dnn.NMSBoxes(boxes, confidences, 0.4, 0.2)
+
+        dets = []
+        if len(idxs) > 0:
+            # loop over the indexes we are keeping
+            for i in idxs.flatten():
+                (x, y) = (boxes[i][0], boxes[i][1])
+                (w, h) = (boxes[i][2], boxes[i][3])
+                dets.append([x, y, x+w, y+h, confidences[i]])
+
+        np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+        dets = np.asarray(dets)
+        tracks = tracker.update(dets)
+
+        boxes = []
+        indexIDs = []
+        c = []
+        previous = memory.copy()
+        memory = {}
+        
+        for track in tracks:
+            boxes.append([track[0], track[1], track[2], track[3]])
+            indexIDs.append(int(track[4]))
+            memory[indexIDs[-1]] = boxes[-1]
+                
+        if len(boxes) > 0:
+            i = int(0)
+            for box in boxes:
+                # extract the bounding box coordinates
+                (x, y) = (int(box[0]), int(box[1]))
+                (w, h) = (int(box[2]), int(box[3]))
+
+                # draw a bounding box rectangle and label on the image
+                # color = [int(c) for c in COLORS[classIDs[i]]]
+                # cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+
+                color = [int(c) for c in COLORS[indexIDs[i] % len(COLORS)]]
+                cv2.rectangle(frame, (x, y), (w, h), color, 2)
+
+                if indexIDs[i] in previous:
+                    previous_box = previous[indexIDs[i]]
+                    (x2, y2) = (int(previous_box[0]), int(previous_box[1]))
+                    (w2, h2) = (int(previous_box[2]), int(previous_box[3]))
+                    p0 = (int(x + (w-x)/2), int(y + (h-y)/2))
+                    p1 = (int(x2 + (w2-x2)/2), int(y2 + (h2-y2)/2))
+                    cv2.line(frame, p0, p1, color, 3)
+
+                    if intersect(p0, p1, line1[0], line1[1]):
+                        detected_class = yolo.counter(p0,out_class, midPoint)
+                        if detected_class == 'car':
+                            car = car + 1
+                        elif detected_class == 'motorbike':
+                            motor = motor+1
+                        elif detected_class == 'bus':
+                            bus = bus+1
+                        else:
+                            car = car + 1
+                            
+                    if intersect(p0, p1, line2[0], line2[1]):
+                        detected_class = yolo.counter(p0,out_class, midPoint)
+                        if detected_class == 'car':
+                            car2 = car2 + 1
+                        elif detected_class == 'motorbike':
+                            motor2 = motor2+1
+                        elif detected_class == 'bus':
+                            bus2 = bus2+1
+                        else:
+                            truck2 = truck2 + 1
+                    '''        
+                    if p0[0] > 921 and p0[0] < 1440 and p0[1] < 606 and p0[1] > 437:
+                        p0Memory = p0
+                        state = True
+                        if 
+                    '''        
+                # text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+                text = "{}".format(indexIDs[i])
+                #cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+                i += 1
+                
+        frame = Image.fromarray(frame)
+        draw = ImageDraw.Draw(frame)
+        draw.text((150,6), 'car %d \nmotor %d \nbus %d \n' %(car,motor,bus), fill=(0, 0, 0),font=font)
+        #draw.text((1328,112), 'car %d \nmotor %d \nbus %d \ntruck %d' %(car2,motor2,bus2,truck2), fill=(255, 255, 255),font=font)
+        
+        frame = np.asarray(frame)
+        # draw line
+        cv2.line(frame, line1[0], line1[1], (0, 255, 255), 1)
+        cv2.line(frame, line2[0], line2[1], (0, 255, 255), 1)
+
+        # draw counter
+        # counter += 1
+        curr_time = timer()
+        exec_time = curr_time - prev_time
+        prev_time = curr_time
+        accum_time = accum_time + exec_time
+        curr_fps = curr_fps + 12
+        if accum_time > 1:
+            accum_time = accum_time - 1
+            fps = "FPS: " + str(curr_fps)
+            curr_fps = 0
+        cv2.putText(frame, text=fps, org=(3, 30), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale=0.8, color=(255, 0, 0), thickness=1)
+        # check if the video writer is None
+        if writer is None:
+            # initialize our video writer
+            fourcc = cv2.VideoWriter_fourcc(*"XVID")
+            video_fps = vs.get(cv2.CAP_PROP_FPS)
+            cv2.putText(frame, 'fps: %d' %(video_fps), (9,20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 5)
+            writer = cv2.VideoWriter('output.avi', fourcc, video_fps, (frame.shape[1], frame.shape[0]), True)
+
+        # write the output frame to disk
+        writer.write(frame)
+        
+        cv2.namedWindow("result for ECL", cv2.WINDOW_NORMAL)
+        cv2.resizeWindow("result for ECL", 904, 544)
+        cv2.imshow('result for ECL',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        # increase frame index
+        frameIndex += 1
+
+    # release the file pointers
+    print("[INFO] cleaning up...")
+    writer.release()
+    vs.release()
+
+if __name__ == '__main__':
+    config = tf.compat.v1.ConfigProto()
+    config.gpu_options.allow_growth = True
+    tf.compat.v1.keras.backend.set_session(tf.compat.v1.Session(config=config))
+    main(YOLO())
